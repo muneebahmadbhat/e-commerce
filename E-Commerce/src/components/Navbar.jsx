3f825d8f2e11716877ab2a5c8d6fdc199bef45ae
muneebahmadbhat <126461917+muneebahmadbhat@users.noreply.github.com>
2025-03-07 16:40:52 +0530

    Search Bar logic added

--- E-Commerce/src/components/Navbar.jsx
+++ E-Commerce/src/components/Navbar.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { assets } from "../assets/assets";
 import { Link, NavLink } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
 
+  const { setShowSearch } = useContext(ShopContext);
+
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <img src={assets.logo} className="w-36" alt="" />
-      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+      <Link to="/">
+        <img src={assets.logo} className="w-36" alt="" />
+      </Link>
+      <ul className="hidden sm:flex gap-5 text-sm ☐ text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1">
           <p>Home</p>
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
@@ -28,7 +33,12 @@ const Navbar = () => {
       </ul>
 
       <div className="flex items-center gap-6">
-        <img src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
+        <img
+          onClick={() => setShowSearch(true)}
+          src={assets.search_icon}
+          className="w-5 cursor-pointer"
+          alt=""
+        />
         <div className="group relative">
           <img
             className="w-5 cursor-pointer"
@@ -36,7 +46,7 @@ const Navbar = () => {
             alt=""
           />
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
-            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 ☐ text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black">My Profile</p>
               <p className="cursor-pointer hover:text-black">Orders</p>
               <p className="cursor-pointer hover:text-black">Logout</p>
